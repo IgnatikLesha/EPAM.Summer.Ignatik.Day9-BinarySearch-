c64fe74175ce4e27dcc435e1bc1ed3e9dfe8179b
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-18 17:32:05 +0300

    + overloads

--- BinarySearch/BinarySearchMethod.cs
+++ BinarySearch/BinarySearchMethod.cs
@@ -8,7 +8,7 @@ namespace BinarySearch
 {
         public static class BinarySearchMethod
         {
-            public static int BinarySearch<T>(T[] array, T elem, IComparer<T> comparer)
+            public static int BinarySearch<T>(T[] array, T elem, int leftIndex, int rightIndex, IComparer<T> comparer)
             {
             if (ReferenceEquals(array, null) || ReferenceEquals(comparer, null) || 
                 ReferenceEquals(elem, null))
@@ -18,7 +18,7 @@ namespace BinarySearch
 
             int i = -1;
 
-                int left = 0, right = array.Length, mid = array.Length/2;
+                int left = leftIndex, right = rightIndex, mid = (leftIndex + rightIndex) / 2;
                 while (left != right)
                 {
                     int temp = comparer.Compare(array[mid], elem);
@@ -38,6 +38,28 @@ namespace BinarySearch
             
             return i;
             }
+
+            public static int BinarySearch<T>(T[] array, T elem, IComparer<T> comparer)
+            {
+            if (ReferenceEquals(array, null) || ReferenceEquals(comparer, null) ||
+                ReferenceEquals(elem, null))
+                throw new ArgumentException();
+            return BinarySearch(array, elem, 0, array.Length - 1, comparer);
+            }
+
+             public static int BinarySearch<T>(T[] array, T elem, Comparison<T> comparer)
+             {
+                  if (array == null)
+                      throw new ArgumentNullException();
+                  return BinarySearch(array, elem, 0, array.Length - 1, new Adapter<T>(comparer));
+             }
+
+             public static int BinarySearch<T>(T[] array, T elem, int leftIndex, int rightIndex, Comparison<T> comparer)
+             {
+                 if (array == null)
+                     throw new ArgumentNullException();
+                 return BinarySearch(array, elem, leftIndex, rightIndex, new Adapter<T>(comparer));
+             }
 
         private static void Sort<T>(T[] array, IComparer<T> comparer)
         {
@@ -61,6 +83,21 @@ namespace BinarySearch
             T temp = lhs;
             lhs = rhs;
             rhs = temp;
+        }
+
+        private class Adapter<T> : IComparer<T>
+        {
+            private readonly Comparison<T> comparison;
+
+            public Adapter(Comparison<T> comparer)
+            {
+                comparison = comparer;
+            }
+
+            public int Compare(T lhs, T rhs)
+            {
+                return comparison(lhs, rhs);
+            }
         }
 
     }
